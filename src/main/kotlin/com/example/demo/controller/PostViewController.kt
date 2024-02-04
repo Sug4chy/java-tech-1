391e34c464f2ad4b1318f543e8aa0246599f6d7e
Степан <sb.9226981433@gmail.com>
2024-02-05 03:41:09 +0500

    added 'single' method to controller

--- src/main/kotlin/com/example/demo/controller/PostViewController.kt
+++ src/main/kotlin/com/example/demo/controller/PostViewController.kt
@@ -1,6 +1,7 @@
 package com.example.demo.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
@@ -14,4 +15,9 @@ class PostViewController {
         return "Здесь будет главная страница"
     }
 
+    @ResponseBody
+    @RequestMapping(path = ["/post/{id}"], method = [RequestMethod.GET])
+    fun single(@PathVariable id: Long): String {
+        return "Здесь будет страница поста №${id}"
+    }
 }
